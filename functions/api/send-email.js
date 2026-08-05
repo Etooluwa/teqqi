baeb1e5791b92e0477565add197c56e1592b3ad1
Esther Segun <esthersegun@mac.home>
2026-08-04 22:48:18 -0400

    Fix contact form sender domain

--- functions/api/send-email.js
+++ functions/api/send-email.js
@@ -13,7 +13,7 @@ export async function onRequestPost(context) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                from: 'Teqqi Website <info@contact.theteqqi.com>',
+                from: 'TEQQI Website <info@theteqqi.com>',
                 to: ['eto@byteqqi.com'],
                 subject: `New Contact Form Submission from ${name}`,
                 html: `
